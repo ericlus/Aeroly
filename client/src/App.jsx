f39ba9786fe76f43feb9bbfa8edd2b18b344d955
Eric Luu <ericluu24@gmail.com>
2019-10-03 19:10:04 -0400

    Added date depart and return date selection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Search from "./Search.jsx";
+import moment from "moment";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Aeroly</h1>
-      <Search />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fromDestination: "",
+      toDestination: "",
+      departDate: moment(new Date()).format("YYYY-MM-DD"),
+      returnDate: moment(new Date())
+        .add("days", 7)
+        .format("YYYY-MM-DD")
+    };
+    this.changeFromDestination = this.changeFromDestination.bind(this);
+    this.changeToDestination = this.changeToDestination.bind(this);
+    this.changeDepartDate = this.changeDepartDate.bind(this);
+    this.changeReturnDate = this.changeReturnDate.bind(this);
+  }
+
+  changeFromDestination(destination) {
+    this.setState({ fromDestination: destination });
+  }
+
+  changeToDestination(destination) {
+    this.setState({ toDestination: destination });
+  }
+
+  changeDepartDate(date) {
+    this.setState({ departDate: date });
+  }
+
+  changeReturnDate(date) {
+    this.setState({ returnDate: date });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Aeroly</h1>
+        <Search
+          changeFromDestination={this.changeFromDestination}
+          changeToDestination={this.changeToDestination}
+          changeDepartDate={this.changeDepartDate}
+          changeReturnDate={this.changeReturnDate}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -1,6 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import { AsyncTypeahead } from "react-bootstrap-typeahead";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
+import moment from "moment";
+import Button from "@material-ui/core/Button";
 
 class Search extends React.Component {
   constructor(props) {
@@ -9,12 +17,16 @@ class Search extends React.Component {
       allowNew: false,
       isLoading: false,
       options: [],
-      from: "",
-      to: ""
+      departDate: new Date(),
+      returnDate: moment(new Date())
+        .add("days", 7)
+        .format("MM-DD-YYYY")
     };
     this.handleSearch = this.handleSearch.bind(this);
     this.handleFromChange = this.handleFromChange.bind(this);
     this.handleToChange = this.handleToChange.bind(this);
+    this.handleDepartDateChange = this.handleDepartDateChange.bind(this);
+    this.handleReturnDateChange = this.handleReturnDateChange.bind(this);
   }
 
   handleSearch(query) {
@@ -30,12 +42,26 @@ class Search extends React.Component {
   }
 
   handleFromChange(input) {
-    this.setState({ from: input[0].PlaceId });
+    this.props.changeFromDestination(input[0].PlaceId);
   }
 
   handleToChange(input) {
-    this.setState({ to: input[0].PlaceId });
+    this.props.changeToDestination(input[0].PlaceId);
   }
+
+  handleDepartDateChange(date, dateFormatted) {
+    this.setState({ departDate: dateFormatted });
+    let departFormatted = moment(dateFormatted).format("YYYY-MM-DD");
+    this.props.changeDepartDate(departFormatted);
+  }
+
+  handleReturnDateChange(date, dateFormatted) {
+    this.setState({ returnDate: dateFormatted });
+    let returnFormatted = moment(dateFormatted).format("YYYY-MM-DD");
+    this.props.changeReturnDate(returnFormatted);
+  }
+
+  handleClick() {}
 
   render() {
     return (
@@ -64,6 +90,43 @@ class Search extends React.Component {
           placeholder="Search for a location"
           onChange={this.handleToChange}
         />
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container justify="space-around">
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Depart"
+              value={this.state.departDate}
+              onChange={this.handleDepartDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date"
+              }}
+            />
+          </Grid>
+        </MuiPickersUtilsProvider>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container justify="space-around">
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Return"
+              value={this.state.returnDate}
+              onChange={this.handleReturnDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date"
+              }}
+            />
+          </Grid>
+        </MuiPickersUtilsProvider>
+        <Button variant="outlined" onClick={this.handleClick}>
+          Search Flights
+        </Button>
       </div>
     );
   }
