4e0b39df049f49aa333cacbc272150fe7f5ff68b
Eric Luu <ericluu24@gmail.com>
2019-10-03 11:38:42 -0400

    Static search autocomplete feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,11 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Search from "./Search.jsx";
 
 const App = () => {
-  useEffect(() => {
-    axios
-      .get(
-        "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-10-10?inboundpartialdate=2019-12-01",
-        {
-          params: {},
-          headers: {
-            "X-RapidAPI-Host":
-              "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-            "X-RapidAPI-Key":
-              "576ba02991msh40bfe0a4e429dc4p1fe089jsn5a1c3ff524a1"
-          }
-        }
-      )
-      .then(result => {
-        console.log(result);
-      });
-  }, []);
   return (
     <div>
-      <div>Hello World</div>
+      <h1>Aeroly</h1>
+      <Search />
     </div>
   );
 };
